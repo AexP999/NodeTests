40a4a4cfe048e257ba35f0e91fa2639d431d597a
Alex <mikol999@gmail.com>
2022-05-26 22:31:29 +0300

    8

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log('Server request');
-  console.log('Just for test');
-  // console.log(req.url, req.method);
 
   res.setHeader('Content-Type', 'application/json');
-  // res.setHeader('Content-Type', 'html');
-
-  // res.write('<head><link rel= "stylesheet" href="#"></head>');
-
-  // res.write('<h1>Hello</h1>');
-  // res.write('<p>I am here</p>');
 
   const data = JSON.stringify([
-    { name: 'Tom', age: 35 },
-    { name: 'Art', age: 32 },
+    { name: 'Tommy', age: 35 },
+    { name: 'Arthur', age: 40 },
   ]);
+  
+  // res.write('<head><link rel="stylesheet" href="#"></head>');
+  // res.write('<h1>Hello world!</h1>');
+  // res.write('<p>My name is Yauhen</p>');
+
 
   res.end(data);
 });
 
 server.listen(PORT, 'localhost', (error) => {
-  error ? console.log(error) : console.log(`Listening port ${PORT}`);
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
