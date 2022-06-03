ce5bdb3723091e9358353d269955351fac1caf3b
Alex <mikol999@gmail.com>
2022-06-03 13:32:10 +0300

    end13

--- server.js
+++ server.js
@@ -1,28 +1,57 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const appp = express();
 
+appp.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 appp.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`Listening port ${PORT}`);
 });
 
+appp.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms')
+);
+
+appp.use(express.static('styles'));
+
 appp.get('/', (req, res) => {
-  res.sendFile(createPath('index'));
+  const title = 'Home';
+  res.render(createPath('index'), { title });
 });
 
 appp.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'));
+  const title = 'Contacts';
+  const contacts = [
+    { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+    { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
+    { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
+  ];
+  res.render(createPath('contacts'), { contacts, title });
 });
 
-appp.get('/about-us', (req, res) => {
-  res.redirect('/contacts');
+appp.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  res.render(createPath('post'), { title });
+});
+
+appp.get('/posts', (req, res) => {
+  const title = 'Posts';
+  res.render(createPath('posts'), { title });
+});
+
+appp.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), { title });
 });
 
 appp.use((req, res) => {
-  res.status(404).sendFile(createPath('error'));
+  const title = 'Error';
+  res.status(404).render(createPath('error'), { title });
 });
